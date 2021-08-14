578f1ff4d43365c4edaec09b48ed93ccd7267481
Vladislav Belov <vladislavbelovdev@gmail.com>
2021-08-14 17:06:38 +0300

    Upgrade command line argument parsing, a little clean up

--- transifex-stats.py
+++ transifex-stats.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-# 
+#
 # The MIT License (MIT)
-# 
-# Copyright (c) 2016 Vladislav Belov
-# 
+#
+# Copyright (c) 2016-2021 Vladislav Belov
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,13 +21,14 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-# 
+#
 
 __author__ = 'vladislavbelov'
 __license__ = 'MIT'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
+import argparse
 import base64
 import getpass
 import json
@@ -52,18 +53,19 @@ UPDATE_TIME = 60 * 15
 
 class Translation(object):
     sort_by = None
-    def __init__(self, source_string, last_update, user, resource):
+    def __init__(self, source_string, last_update, user, resource, translated_string):
         self.source_string = source_string
         self.last_update = last_update
         self.user = user
         self.resource = resource
+        self.translated_string = translated_string
 
     def __lt__(self, other):
         return self.less(other)
-    
+
     def __cmp__(self, other):
         return self.less(other)
-    
+
     def less(self, other):
         if Translation.sort_by is None:
             return self.last_update > other.last_update
@@ -75,31 +77,32 @@ class Translation(object):
             return self.source < other.source
         else:
             return self.last_update > other.last_update
-    
+
     @staticmethod
     def sort_by(group):
         Translation.sort_by = group
+
 
 class User(object):
     def __init__(self, name):
         self.name = name
         self.count = 0
         self.last_update = '1970-01-01T00:00:00.000'
-    
+
     def add_translation(self, translation):
         self.count += 1
         if translation['last_update'] > self.last_update:
             self.last_update = translation['last_update']
-    
+
     def __lt__(self, other):
         return self.count > other.count
-    
+
     def __cmp__(self, other):
         return self.count > other.count
 
 
 class TransifexStats(object):
-    
+
     def __init__(self, project, username, language='en', password=None, quiet=False, logoutput=sys.stdout):
         # project authorization data
         self.project = project
@@ -108,7 +111,7 @@ class TransifexStats(object):
         self.language = language
         self.quiet = quiet
         self.logoutput = logoutput
-        
+
         # project translation data
         self.resources = None
 
@@ -125,7 +128,7 @@ class TransifexStats(object):
             'Content-Type': 'application/json',
             'User-Agent': 'Statsbot/%s' % (__version__)
         }
-        
+
         try:
             request = Request(url, None, headers)
             handle = urlopen(request)
@@ -137,15 +140,15 @@ class TransifexStats(object):
                 exit()
             else:
                 raise
-        
+
         return data
-    
+
     def need_password(self, cache=True):
         path = '%s_resources_%s.json' % (self.project, self.language)
         if cache and os.path.isfile(path) and time.time() - os.path.getmtime(path) <= UPDATE_TIME:
             return False
         return True
-    
+
     def download(self, cache=True):
         path = '%s_resources_%s.json' % (self.project, self.language)
         if cache and os.path.isfile(path) and time.time() - os.path.getmtime(path) <= UPDATE_TIME:
@@ -154,18 +157,18 @@ class TransifexStats(object):
             self.resources = json.load(handle)
             handle.close()
             return
-        
+
         self.log('Downloading resource list:')
         self.resources = self.request('/project/%s/resources/' % self.project)
         self.log(' Completed.')
-        
+
         self.log('Downloading resources strings for language "%s":' % (self.language))
         for resource in self.resources:
             self.log(' Resource: %s' % (resource['name']))
             method = '/project/%s/resource/%s/translation/%s/strings/?details' % (self.project, resource['slug'], self.language)
             resource['strings'] = self.request(method)
             self.log('  Completed.')
-        
+
         if cache:
             handle = open(path, 'w')
             json.dump(self.resources, handle)
@@ -181,20 +184,20 @@ class TransifexStats(object):
                     continue
                 if user not in users:
                     users[user] = User(user)
-                translation = Translation(str['source_string'], str['last_update'], str['user'], resource['name'])
+                translation = Translation(str['source_string'], str['last_update'], str['user'], resource['name'], str['translation'])
                 users[user].add_translation({'last_update': str['last_update']})
                 translations.append(translation)
-        
+
         if group:
             Translation.sort_by(group)
         users = sorted(users.values())
         translations = sorted(translations)
-        
+
         # top users
         top_limit = 50
         if 'top_limit' in limits:
             top_limit = int(limits['top_limit'])
-        
+
         path = '%s_%s_users_top_%d.txt' % (self.project, self.language, top_limit)
         handle = open(path, 'w')
         line = '%21s %18s %21s\n' % ('user name', 'translations', 'last update')
@@ -206,9 +209,9 @@ class TransifexStats(object):
             except:
                 handle.write(line.encode('utf-8'))
         handle.close()
-        
+
         print('Top %d user list saved to "%s"' % (top_limit, path))
-        
+
         # last changes
         changes_limit = 100
         if 'changes_limit' in limits:
@@ -217,6 +220,7 @@ class TransifexStats(object):
         path = '%s_%s_last_changes.txt' % (self.project, self.language)
         handle = open(path, 'w')
         handle.write('Last changes\n\n')
+
         for translation in translations[:changes_limit]:
             line = '%s: "%s", %s by %s\n' % (translation.resource, translation.source_string, translation.last_update, translation.user)
             try:
@@ -224,68 +228,43 @@ class TransifexStats(object):
             except:
                 handle.write(line.encode('utf-8'))
         handle.close()
-        
+
         print('Last %d changes list saved to "%s"' % (changes_limit, path))
-        
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('projectcode', help='project code')
+    parser.add_argument('username', help='username of an account on the transifex')
+    parser.add_argument('-p', '--password', help='password of the account on the transifex', default=None)
+    parser.add_argument('-l', '--language', help='language code of the project on the transifex', default=None)
+    parser.add_argument('-g', '--groupby', choices=['resource', 'user', 'source', 'date'],
+        help='group changes by param (resource, user, source, date - default)', default=None)
+    parser.add_argument('-s', '--limits', help='set limits (top_limit, changes_limit', default=None)
+    args = parser.parse_args()
+
+    password = args.password
+    language = args.language
+
+    limits = {}
+    if args.limits:
+        pair = args.limits.split('=')
+        limits[pair[0]] = pair[1]
+
+    stats = TransifexStats(args.projectcode, args.username)
+
+    if not language:
+        language = raw_input('Language code: ')
+    stats.language = language
+
+    if not password and stats.need_password():
+        password = getpass.getpass('Password: ')
+    stats.password = password
+
+    stats.download()
+    stats.analyze(limits, args.groupby)
 
 
 if __name__ == '__main__':
-    def help():
-        print('usage: transifex-stats -p project -u username [-p password -l language -s top_limit=50]')
-        print(' -i\tproject code')
-        print(' -u\tusername of the account on the transifex')
-        print(' -p\tpassword of the account on the transifex')
-        print(' -l\tlanguage code of the project on the transifex')
-        print(' -g\tgroup changes by param (resource, user, source, date - default)')
-        print(' -s\tset limits (top_limit, changes_limit')
-    
-    def parse():
-        if len(sys.argv) == 1:
-            help()
-            return
-        
-        project = None
-        username = None
-        password = None
-        language = None
-        limits = {}
-        group = None
-        
-        for i in range(len(sys.argv)):
-            key = sys.argv[i]
-            value = None
-            if i + 1 < len(sys.argv):
-                value = sys.argv[i + 1]
-            if key == '-i':
-                project = value
-            elif key == '-u':
-                username = value
-            elif key == '-p':
-                username = value
-            elif key == '-l':
-                language = value
-            elif key == '-g':
-                group = value
-            elif key == '-s':
-                pair = value.split('=')
-                limits[pair[0]] = pair[1]
-        
-        if not project or not username:
-            help()
-            return
-        
-        stats = TransifexStats(project, username)
-        
-        if not language:
-            language = raw_input('Language code: ')
-        stats.language = language
-        
-        if not password and stats.need_password():
-            password = getpass.getpass('Password: ')
-        stats.password = password
-        
-        stats.download()
-        stats.analyze(limits, group)
-    
-    parse()
-    
+    run()
+
